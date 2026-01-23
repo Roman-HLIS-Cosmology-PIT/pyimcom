a2a6c937c08a7a111a430f05bf2b957a55f4d509
hirata10 <hirata.10@osu.edu>
2026-01-22 22:57:25 -0500

    Create test_imdestripe.py
    This is a place for test functions for `imdestripe.py` (I added one just to get started).

---
+++ tests/pyimcom/test_imdestripe.py
@@ -0,0 +1,13 @@
+"""Test functions for imdestripe."""
+
+from pyimcom import imdestripe
+
+def test_get_ids():
+    """Test function for splitting an obsid,sca pair."""
+
+    s = "670_16"  # string to parse
+    obsid, scaid = imdestripe.get_ids(s)
+    print(obsid, scaid)
+    # check if we parsed correctly
+    assert obsid == 670
+    assert scaid == 16
