60f814c96cffe8fb4898379819751daa323b5406
hirata10 <hirata.10@osu.edu>
2026-01-22 23:10:20 -0500

    Update test_imdestripe.py
    fixed test to reflect what the function is supposed to do

--- tests/pyimcom/test_imdestripe.py
+++ tests/pyimcom/test_imdestripe.py
@@ -6,9 +6,9 @@ from pyimcom import imdestripe
 def test_get_ids():
     """Test function for splitting an obsid,sca pair."""
 
-    s = "670_16"  # string to parse
+    s = "mybeginning_670_16.myending"  # string to parse
     obsid, scaid = imdestripe.get_ids(s)
     print(obsid, scaid)
     # check if we parsed correctly
-    assert obsid == 670
-    assert scaid == 16
+    assert obsid == "670"
+    assert scaid == "16"
