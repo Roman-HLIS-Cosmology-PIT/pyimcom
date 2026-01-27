58185cba383b616c0c0085b334eb8387a5de61e7
hirata10 <hirata.10@osu.edu>
2026-01-26 20:12:42 -0500

    Update test_fname.py
    another ruff fix

--- tests/pyimcom/test_fname.py
+++ tests/pyimcom/test_fname.py
@@ -7,11 +7,11 @@ def test_fname_errs():
     """Test exceptions: file name errors."""
 
     try:
-        c = CompressedOutput("unknown_file")
+        c = CompressedOutput("unknown_file")  # noqa: F841
     except Exception as e:
         assert str(e) == "unrecognized file type"
 
     try:
-        c = ReadFile("notascheme://testonly/test.test")
+        c = ReadFile("notascheme://testonly/test.test")  # noqa: F841
     except ValueError as e:
         assert str(e) == "Scheme notascheme not supported"
