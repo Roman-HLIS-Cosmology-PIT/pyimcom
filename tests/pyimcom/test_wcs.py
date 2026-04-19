04c10fa710d4efbdfded3a913232e48a62d8a4c1
hirata10 <hirata.10@osu.edu>
2026-04-19 16:26:26 -0400

    test of GWCS-native functionality in PyIMCOM_WCS
    We aren't planning to use this, but it is an option just in case, and I wanted to make sure it has a unit test.

--- tests/pyimcom/test_wcs.py
+++ tests/pyimcom/test_wcs.py
@@ -1,7 +1,11 @@
+import gwcs
 import numpy as np
+import pytest
+from astropy import coordinates as coord, models, units as u
 from astropy.io import fits
 from astropy.wcs import WCS
-from pyimcom.wcsutil import get_pix_area
+from gwcs import coordinate_frames as cf
+from pyimcom.wcsutil import ABasis, PyIMCOM_WCS, get_pix_area
 
 
 def test_fits(tmp_path):
@@ -69,4 +73,42 @@ def test_fits(tmp_path):
     assert np.max(np.amax(err)) < 3e-6
 
 
-# test_fits("out") # <-- comment out
+def test_gwcs():
+    """Test for initializing from a GWCS, and running from pixel -> world -> back."""
+
+    # coordinates
+    crpix = np.array([-254.695456590193, 819.255060728745])
+    cd = np.array([
+        [3.08219178082192e-05, 1.22309197651664e-07],
+        [0, -3.02103718199609e-05],
+    )
+
+    # pipeline version of the WCS
+    distortion = models.AffineTransformation2D(cd, translation=[0, 0])
+    distortion.inverse = models.AffineTransformation2D(np.linalg.inv(cd), translation=[0, 0])
+    celestial_rotation = models.RotateNative2Celestial(9.55, -41.0, 180.0)
+    shift = models.Shift(-(crpix[0] - 1)) & models.Shift(-(crpix[1] - 1))
+    det2sky = (shift | distortion | models.Pix2Sky_ARC() | celestial_rotation)
+    det2sky.name = "TestMapping1"
+    detector_frame = cf.Frame2D(name="detector", axes_names=("x", "y"), unit=(u.pix, u.pix))
+    sky_frame = cf.CelestialFrame(reference_frame=coord.ICRS(), name="icrs", unit=(u.deg, u.deg))
+    sca_gwcs = gwcs.WCS([(detector_frame, det2sky), (sky_frame, None)])
+
+    # PyIMCOM version of this
+    pyimcom_wcs = PyIMCOM(sca_gwcs, noconvert=True)
+    x = 256.0
+    y = 512.0
+    ra, dec = pyimcom_wcs.all_pix2world(x, y)
+    ra2, dec2 = sca_gwcs.pixel_to_world_values([x], [y])
+    print(ra, dec)
+    assert np.hypot(ra - ra2[0], dec - dec2[0]) < 1.0e-5  # check pixel mapping
+    xx, yy = pyimcom_wcs.all_world2pix(ra, dec)
+    assert np.hypot(x - xx, y - yy) < 0.01  # check pixel mapping
+
+
+def test_special_cases():
+    """Test edge cases for WCS."""
+
+    ABasis(6, 1000).coef_setup()  # should just pass through
+    with pytest.raises(TypeError):
+        PyIMCOM_WCS(0)  # can't give it an integer
